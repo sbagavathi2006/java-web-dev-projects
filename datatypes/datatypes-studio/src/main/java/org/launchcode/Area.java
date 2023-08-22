34c8d209993cc401b37a9dafb564335edb6393e3
Bagavathi <sbagavathi2006@gmail.com>
2023-08-21 20:26:08 -0500

    completed

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -4,9 +4,16 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius +" is "+area);
-        input.close();
+
+        if(input.hasNextDouble()){
+            double radius = input.nextDouble();
+            if(radius<0){
+                System.err.println("Enter a valid radius (in positive number): ");
+            }
+            System.out.println("The area of a circle of radius " + radius +" is "+Circle.getArea(radius));
+            input.close();
+            } else{
+            System.err.println("Enter a valid radius (in positive number): ");
+        }
     }
 }
